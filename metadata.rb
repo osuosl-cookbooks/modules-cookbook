52bdf523125e10dd920826b677e6e8aaf6a77e6d
Jordan Evans <jordane@osuosl.org>
2015-06-16 16:50:51 -0700

    bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'guilhem.lettron@youscribe.com'
 license          'Apache v2.0'
 description      'Manage modules on linux'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.3'
+version          '0.2.4'
 supports         'ubuntu'
 supports         'centos', '>= 6.0'
 supports         'redhat', '>= 6.0'
